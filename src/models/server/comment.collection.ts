040cdd32adb2b40aef2a92826dd1f70eeda88caa
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-26 19:32:49 -0600

    chore: Create comment collection with permissions and attributes

---
+++ src/models/server/comment.collection.ts
@@ -0,0 +1,25 @@
+import { Permission } from 'node-appwrite';
+import {db, commentCollection, answerCollection} from '../name';
+import { databases } from './config';
+
+export async function createCommentCollection(){
+    await databases.createCollection(db, commentCollection, "commentCollection", [
+        Permission.read('any'),
+        Permission.create('users'),
+        Permission.read('users'),
+        Permission.update('users'),
+        Permission.delete('users')
+    ])
+
+    console.log("Comments collection created successfully");
+
+    //creating answer attributes
+    await Promise.all([
+        // databases.createEnumAttribute(db, commentCollection, )
+        databases.createStringAttribute(db, commentCollection, "content", 1000, true),
+        databases.createStringAttribute(db, commentCollection, "typeId", 50, true),
+        databases.createStringAttribute(db, commentCollection, "authorId", 50, true)
+    ])
+
+    console.log("Comment attributes created successfully");
+}
