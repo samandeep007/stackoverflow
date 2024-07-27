5ed8569a7653a0702d39495f4ab699f2661682fe
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-26 19:38:58 -0600

    chore: Create vote collection with permissions and attributes

---
+++ src/models/server/vote.collection.ts
@@ -0,0 +1,24 @@
+import { Permission } from 'node-appwrite';
+import {db, voteCollection} from '../name';
+import { databases } from './config';
+
+export async function createVoteCollection(){
+    await databases.createCollection(db, voteCollection, voteCollection, [
+        Permission.read('any'),
+        Permission.create('users'),
+        Permission.read('users'),
+        Permission.update('users'),
+        Permission.delete('users')
+    ])
+
+    console.log("Vote collection created successfully");
+
+    await Promise.all([
+        databases.createStringAttribute(db, voteCollection, "votedById", 50, true),
+        databases.createStringAttribute(db, voteCollection, "typeId", 50, true),
+        // databases.createStringAttribute(db, voteCollection, )
+        // databases.createEnumAttribute()
+    ])
+
+    console.log("vote attributes created successfully");
+}
