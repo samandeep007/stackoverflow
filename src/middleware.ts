ef705a092106a76e799d59c4fab65d69458145b8
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-26 20:49:36 -0600

    Refactor middleware to initialize database and storage on request

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,26 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import getOrCreateDB from './models/server/dbSetup'
+import getOrCreateStorage from './models/server/storageSetup'
  
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+export async function middleware(request: NextRequest) {
+    await Promise.all([
+        getOrCreateDB(),
+        getOrCreateStorage()
+    ])
+  return NextResponse.next();
 }
  
-// See "Matching Paths" below to learn more
+// See "Matching Paths" below to learn more --> Wherever the matcher matches the path, middleware will not run there
 export const config = {
-  matcher: '/about/:path*',
+/* match all the request paths except for the ones that starts with:
+ -- api
+ -- _next/static
+ -- _next/image
+ -- favicon.ico
+*/
+  matcher: [
+    "/api(/.*)?|/_next/(static|image)(/.*)?|/favicon\.ico"
+  ],
 }
