d5f2f6297d237b353706139f50be60abd2f58e48
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-26 19:26:37 -0600

    feat: Create answer and question collections with permissions and attributes

---
+++ src/models/server/answer.collection.ts
@@ -0,0 +1,26 @@
+import { Permission } from 'node-appwrite';
+import { db, answerCollection } from '../name';
+import { databases } from './config';
+
+export default async function createAnswerCollection() {
+
+    await databases.createCollection(db, answerCollection, "answerCollection", [
+        Permission.read('any'),
+        Permission.create('users'),
+        Permission.read('users'),
+        Permission.update('users'),
+        Permission.delete('users')
+    ])
+
+    console.log("Answer collection created successfully");
+
+    // create attributes
+    await Promise.all([
+        databases.createStringAttribute(db, answerCollection, "content", 1000, true),
+        databases.createStringAttribute(db, answerCollection, "questionId", 50, true),
+        databases.createStringAttribute(db, answerCollection, "authorId", 50, true)
+    ])
+
+    console.log("Answer attributes created successfully");
+    
+}
